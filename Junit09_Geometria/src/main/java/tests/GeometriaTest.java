e6b421a666f1cd1e72eb70b08efd0d56b8250e02
pbesora <pau_besora@hotmail.com>
2022-08-05 09:44:15 +0200

    Test class created

--- Junit09_Geometria/src/main/java/tests/GeometriaTest.java
+++ Junit09_Geometria/src/main/java/tests/GeometriaTest.java
@@ -1,9 +1,38 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
-import Geometria;
+
+import JUnit.Junit09_Geometria.dto.Geometria;
 
 public class GeometriaTest {
 
+	
+	@Test
+	public void testAreaCuadrado() {
+		int resultado = Geometria.areaCuadrado(2);
+		int esperado = 4;
+		
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testAreaCirculo() {
+		double resultado = Geometria.areaCirculo(2);
+		double esperado = 12.5664;
+		
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testAreaTriangulo() {
+		int resultado = Geometria.areaTriangulo(2, 5);
+		int esperado = 5;
+		
+		
+		assertEquals(esperado, resultado);
+	}
 }
